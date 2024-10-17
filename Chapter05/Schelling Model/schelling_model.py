4e06e56d553f634dc039e2dcb82d149f96317797
unknown <elhamjafari200312@gmail.com>
2024-10-17 11:11:35 +0330

    Add Schelling Model Code

--- Chapter05/Schelling Model/schelling_model.py
+++ Chapter05/Schelling Model/schelling_model.py
@@ -26,3 +26,71 @@ class Agent:
     def update_location(self, agents, neighbor_count, threshold):
         while not self.is_satisfied(agents, neighbor_count, threshold):
             self.location = self._random_location()
+
+class Simulation:
+    def __init__(self, num_agents_A, num_agents_B, neighbor_count, threshold):
+        self.num_agents_A = num_agents_A
+        self.num_agents_B = num_agents_B
+        self.neighbor_count = neighbor_count
+        self.threshold = threshold
+        self.agents = self._create_agents()
+
+    def _create_agents(self):
+        agents = [Agent(0) for _ in range(self.num_agents_A)]
+        agents.extend(Agent(1) for _ in range(self.num_agents_B))
+        return agents
+
+    def run(self):
+        step = 0
+        while True:
+            self.plot_grid(step)
+            print("\n")
+            same_location_count = 0
+            for agent in self.agents:
+                old_location = agent.location
+                agent.update_location(self.agents, self.neighbor_count, self.threshold)
+                if agent.location == old_location:
+                    same_location_count += 1
+
+            if same_location_count == len(self.agents):
+                break
+            step += 1
+        
+        print(f'\n\n ---> Satisfied agents with {self.threshold / self.neighbor_count * 100}% of similar neighbors')
+
+    def plot_grid(self, step):
+      x_A, y_A = [], []
+      x_B, y_B = [], []
+
+      for agent in self.agents:
+          x, y = agent.location
+          if agent.agent_type == 0:
+              x_A.append(x)
+              y_A.append(y)
+          else:
+              x_B.append(x)
+              y_B.append(y)
+
+      fig, ax = plt.subplots(figsize=(10, 10))
+
+      if x_A and y_A:
+          ax.plot(x_A, y_A, '^', markerfacecolor='b', markersize=10, label='Type A')
+      if x_B and y_B:
+          ax.plot(x_B, y_B, 'o', markerfacecolor='r', markersize=10, label='Type B')
+
+      ax.set_title(f'Step number = {step}')
+      ax.legend()
+      plt.show()
+
+
+def main():
+    num_agents_A = 500
+    num_agents_B = 500
+    neighbor_count = 8
+    threshold = 4
+
+    simulation = Simulation(num_agents_A, num_agents_B, neighbor_count, threshold)
+    simulation.run()
+
+if __name__ == "__main__":
+    main()
